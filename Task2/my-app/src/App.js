69d019aa1ef5e6a1794463d65fa89394962a872e
RohithParambath <rohithparambath@gmail.com>
2024-08-29 13:57:39 +0530

    Task-2 29/08/2024

---
+++ Task2/my-app/src/App.js
@@ -0,0 +1,12 @@
+import MeaningFinder from "./MeaningFinder";
+
+function App() {
+  return (
+    <div className="App">
+      <MeaningFinder />
+      <p>The Output will be shown in console!</p>
+    </div>
+  );
+}
+
+export default App;
